9d7decda1cf948081a2dddec3848ba55982bd0bc
Murshid Bin Ibrahim <murshidshazz@gmail.com>
2023-03-20 11:56:59 +0530

    Understanding the Functional Components

--- App.js
+++ App.js
@@ -1,55 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* React DOM Manipulation */
+// JSX Expression
 
-/*
-    parcel is doing all the things (Interview Question)
-
-    + Hot Module Replacement (HMR)
-    + File Watcher Algorithm-C++ (Live Server Feature)
-    + BUNDLING 
-    + MINIFYING
-    + Cleaning our Code 
-    + Dev and Production Build
-    + Super Fast Build Algorithm
-    + Image Optimization
-    + Caching while development [after refreshing it will come milliseconds - caching]
-    + Compression
-    + Compatible with older version of browser
-    + HTTPS on Dev.Machine (npx build index.html --https)
-    + Manage port numbers
-    + Consistent Hashing Algorithm
-    + Zero Config
-    + Tree Shaking - removing unwanted code
-
-// Transitive Dependencies
-
-*/ 
-
-//React.createElement
-
-const heading = React.createElement("h1",{id : "title", key : "001"},"Hello World - React.createElement");
-
-// React.createElement ==> Object ==> HTML(DOM)
-
-console.log(heading);
-
-// JSX Express
-
-const heading_jsx = (
-    <h1 id="title" key="002"> Hello World - JSX</h1>
+const Title = () => (
+    <h1 id="title" key="002"> DevMK</h1>
 )
 
-//JSX ==> React.createElement ==> Object ==> HTML(DOM)
+// React Component
 
-console.log(heading_jsx);
+//  1. Functional Component - JS Function, it returns JSX
+//  2. Class Based Component
 
+//Normal Function
 
+const myFriend = "Jaseem"; // We can write any piece of JS Code inside JSX - (It is very secure - Sanitization)
 
-const container = React.createElement("div",{id:"container" , myprops : "props-sample"},[heading, heading_jsx]);
+// + Component composition ==> Component inside a Component (Nested component) = (<Title />)
+
+function HeaderComponent () {
+    return (
+        <div>
+            <Title /> <h2>{myFriend}</h2>
+            <h1>Learn React - Functional Component</h1>
+            <h2>This is a description from Normal Fn</h2>
+        </div>
+    )
+}
+
+// Arrow Function - we don't need to write return & {}
+
+const HeaderComponent_Arrow = () => (
+        <div>
+            <h1>Learn React - Functional Component</h1>
+            <h2>This is a description From Arrow Fn</h2>
+        </div>
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(
+    <HeaderComponent/>
+);
 
