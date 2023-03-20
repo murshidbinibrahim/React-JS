ea6e911fed172d0bff1c1cf29af4ab967b325b84
Murshid Bin Ibrahim <murshidshazz@gmail.com>
2023-03-20 20:06:58 +0530

    Body Component - Filled with Static data

--- App.js
+++ App.js
@@ -27,8 +27,37 @@ const Header = () => {
   );
 };
 
+const mcDonalds = {
+  image:
+    "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/0e8f93a56b93e43d7627c2c21bc1106c",
+  name: "Mc.Donalds",
+  cuisines: ["Burgers", "Cafe", "Desserts"],
+  rating: 4.5,
+};
+
+const RestuarantCard = () => {
+  return (
+    <div className="card">
+      <img src={mcDonalds.image} />
+      <h2>{mcDonalds.name}</h2>
+      <h3>{mcDonalds.cuisines.join(", ")}</h3>
+      <h4>{mcDonalds.rating} stars</h4>
+    </div>
+  );
+};
+
 const Body = () => {
-  return <h1>Body</h1>;
+  return (
+    <div className="restaurant-list">
+      <RestuarantCard />
+      <RestuarantCard />
+      <RestuarantCard />
+      <RestuarantCard />
+      <RestuarantCard />
+      <RestuarantCard />
+      <RestuarantCard />
+    </div>
+  );
 };
 
 const Footer = () => {
@@ -38,7 +67,9 @@ const Footer = () => {
 const AppLayout = () => {
   return (
     <React.Fragment>
-      <Header /> <Body /> <Footer />
+      <Header />
+      <Body />
+      <Footer />
     </React.Fragment>
   );
 };
