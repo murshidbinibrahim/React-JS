b1ae7697f29f06d0e6562655e2334f92af1cb1a2
Murshid Bin Ibrahim <murshidshazz@gmail.com>
2023-03-20 18:21:33 +0530

    Header Component Added

--- App.js
+++ App.js
@@ -1,46 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX Expression
-
 const Title = () => (
-    <h1 id="title" key="002"> DevMK</h1>
-)
+  <a href="/">
+    <img
+      alt="logo"
+      src="https://www.freepnglogos.com/uploads/logo-ifood-png/logo-client-ifood-hd-black-symbol-png-4.png"
+      className="logo"
+    />
+  </a>
+);
 
-// React Component
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-//  1. Functional Component - JS Function, it returns JSX
-//  2. Class Based Component
+const Body = () => {
+  return <h1>Body</h1>;
+};
 
-//Normal Function
+const Footer = () => {
+  return <h1>Footer</h1>;
+};
 
-const myFriend = "Jaseem"; // We can write any piece of JS Code inside JSX - (It is very secure - Sanitization)
-
-// + Component composition ==> Component inside a Component (Nested component) = (<Title />)
-
-function HeaderComponent () {
-    return (
-        <div>
-            <Title /> <h2>{myFriend}</h2>
-            <h1>Learn React - Functional Component</h1>
-            <h2>This is a description from Normal Fn</h2>
-        </div>
-    )
-}
-
-// Arrow Function - we don't need to write return & {}
-
-const HeaderComponent_Arrow = () => (
-        <div>
-            <h1>Learn React - Functional Component</h1>
-            <h2>This is a description From Arrow Fn</h2>
-        </div>
-)
-
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <Header /> <Body /> <Footer />
+    </React.Fragment>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <HeaderComponent/>
-);
-
+root.render(<AppLayout />);
---
+++ Readme.txt
@@ -0,0 +1,17 @@
+> Header
+  + Logo
+  + Nav Items
+  + Cart 
+
+> Body
+  + Search Bar
+  + Restaurant List
+    + Restaurant Card
+      + Res_Image
+      + Res_Name
+      + Rating
+      + Cuisines
+
+> Footer
+  + Links
+  + Copyright
